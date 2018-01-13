6dd0bc2d900c937d9f7a50f00f01dcfdc1558b90
Anikesh <anikeshpatel4@gmail.com>
2018-01-13 13:37:24 +0530

    All Bugs Clear

--- src/self/anikesh/lucid/Main.java
+++ src/self/anikesh/lucid/Main.java
@@ -1,22 +1,37 @@
 package self.anikesh.lucid;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+import java.io.IOException;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("splash.fxml"));
+        FadeTransition transition = new FadeTransition(Duration.seconds(5),root);
+        transition.setFromValue(1);
+        transition.setToValue(0);
         primaryStage.setScene(new Scene(root));
         primaryStage.setTitle("Lucid Smart Buddy");
         primaryStage.setResizable(false);
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.show();
 
+        transition.play();
+        transition.setOnFinished(e->{
+            try {
+                primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("Home.fxml"))));
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
 
     }
 
---
+++ src/self/anikesh/lucid/Splash.java
@@ -0,0 +1,19 @@
+package self.anikesh.lucid;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Splash implements Initializable {
+
+    @FXML
+    private Label userName;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        userName.setText(System.getProperty("user.name"));
+    }
+}
