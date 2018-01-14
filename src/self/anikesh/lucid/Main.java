d43d41849d5e952b019d005a799028f187e9b769
Anikesh <anikeshpatel4@gmail.com>
2018-01-14 10:16:14 +0530

    all done

--- src/self/anikesh/lucid/Main.java
+++ src/self/anikesh/lucid/Main.java
@@ -1,6 +1,7 @@
 package self.anikesh.lucid;
 
 import javafx.animation.FadeTransition;
+import javafx.animation.RotateTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,19 +16,19 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("splash.fxml"));
-        FadeTransition transition = new FadeTransition(Duration.seconds(5),root);
+        FadeTransition transition = new FadeTransition(Duration.seconds(3),root);
         transition.setFromValue(1);
         transition.setToValue(0);
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(new Scene(root,750,420));
         primaryStage.setTitle("Lucid Smart Buddy");
         primaryStage.setResizable(false);
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.show();
-
         transition.play();
         transition.setOnFinished(e->{
             try {
                 primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("Home.fxml"))));
+                System.out.println("DOne");
             } catch (IOException e1) {
                 e1.printStackTrace();
             }
